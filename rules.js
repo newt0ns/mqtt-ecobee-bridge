4795bf77a9ebb15cc79e95d70968ca087a2baa0f
Justin Wood <woody@apple.com>
2017-04-29 14:55:42 -0700

    Squashed 'homeautomation-js-lib/' changes from fefc7e7..6b58352
    6b58352 Update rules
    411de10 Adding emitters
    d5de13e Updating
    
    git-subtree-dir: homeautomation-js-lib
    git-subtree-split: 6b58352abc3e673ccae162a8feea1dfe27bb271e

---
+++ rules.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const EventEmitter = require('events')
+const path = require('path')
+const watch = require('watch')
+const yaml = require('js-yaml')
+const logging = require('./logging.js')
+
+var configs = []
+var config_path = null
+
+module.exports = new EventEmitter()
+
+module.exports.load_path = function(in_path) {
+    config_path = in_path
+        // Watch Path
+    watch.watchTree(config_path, function(f, curr, prev) {
+        logging.log('Updating rules')
+        load_rule_config()
+    })
+}
+
+module.exports.get_configs = function() {
+    return configs
+}
+
+module.exports.ruleIterator = function(callback) {
+    configs.forEach(function(config_item) {
+        Object.keys(config_item).forEach(function(key) {
+            callback(key, config_item[key])
+        }, this)
+    }, this)
+}
+
+function print_rule_config() {
+    configs.forEach(function(config_item) {
+        Object.keys(config_item).forEach(function(key) {
+            logging.log(' Rule [' + key + ']')
+        }, this)
+    }, this)
+}
+
+function load_rule_config() {
+    fs.readdir(config_path, function(err, files) {
+        configs = []
+
+        logging.log('Loading rules at path: ' + config_path)
+        if (err) {
+            throw err
+        }
+
+        files.map(function(file) {
+            return path.join(config_path, file)
+        }).filter(function(file) {
+            return fs.statSync(file).isFile()
+        }).forEach(function(file) {
+            logging.log(' - Loading: ' + file)
+            const doc = yaml.safeLoad(fs.readFileSync(file, 'utf8'))
+            configs.push(doc)
+        })
+
+        logging.log('...done loading rules')
+        print_rule_config()
+        module.exports.emit('rules-loaded')
+    })
+}
